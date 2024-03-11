8dae424ca2d90267cd92fb0deefb207171bae477
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2024-03-11 10:33:08 -0500

    Updated

--- Tecnico/2024/marzo/10-sesion-03-07/ejercicios.py
+++ Tecnico/2024/marzo/10-sesion-03-07/ejercicios.py
@@ -1,3 +1,65 @@
+# 1.El usuario ingresa un número y el programa le devuelve si es par o impar  
+num = 0
+while num == 0:
+        try:
+                num = float(input("Digite un número "))
+
+                if num%2 == 0:
+                        print(num, "Es par")
+                else:
+                        print(num, "Es impar")
+        except:
+                print("Digite un valor númerico")
+                num = 0
+                
+# 2. El usuario ingresa un número y el programa le devuelve si es par o impar y si es positivo o negativo
+
+num = 0
+while num == 0:
+        try:
+                num = float(input("Digite un número "))
+
+                if num % 2 == 0:
+                        print(num, "es par")
+                else:
+                        print(num, "es impar")
+        
+                if num > 0:
+                        print(num, "es positivo")
+                else:
+                        print(num, "es negativo")
+        except:
+                print("Digite un valor númerico")
+                num = 0
+                
+# 3. Si el segundo número es negativo y el primero es impar imprimir el mayor de lo contrario el menor 
+
+
+num = 0
+while num == 0:
+        try:
+                num = float(input("Digite un número "))
+                num2 = float(input("Digite un número "))
+                
+
+                if num2 < 0 and num % 2 != 0:
+                        if num2 > num:
+                                print(num2, ",", num)
+                        else:
+                                print(num, ",", num2)
+                else:
+                        if num > num2:
+                                print(num2, ",", num)
+                        else:
+                                print(num, ",", num2)
+        
+        except:
+                print("Digite un valor númerico")
+                num = 0
+
+# 4. El usuario ingresa 3 números y el programa le indica cual es el mayor, medio y menor
+        
+        
 msgMayor=("Este es el dato mayor")
 msgMenor=("Este es el dato medio")
 msgMedio=("Este es el dato menor")
